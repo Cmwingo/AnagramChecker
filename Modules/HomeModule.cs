62800357fddc66b202c3d8de95641414880f4263
Chris Wingo and Ryan Peterson <chris+ryan@>
2016-11-29 14:01:26 -0800

    add styling

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,7 +9,13 @@ namespace Anagram
     public HomeModule()
     {
       Get["/"] = _ =>{
+        //List<string> anagramList = new List<string>();
         return View["index.cshtml"];
+      };
+      Post["/anagram"] = _ =>{
+        AnagramChecker newAnagram = new AnagramChecker(Request.Form["user-word"], Request.Form["user-anagrams"]);
+        var anagramList = newAnagram.Check();
+        return View["anagram.cshtml", anagramList];
       };
     }
   }
